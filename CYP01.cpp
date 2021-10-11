6537015f9c38e86ca7230a2b67e95e70be17f93f
Zaira <92006681+ZairaCR@users.noreply.github.com>
2021-10-11 13:59:35 -0500

    Se agrega un cambio desde GITHUB

--- CYP01.cpp
+++ CYP01.cpp
@@ -2,9 +2,10 @@
 
 int main()
 {
+	//Numero de cuenta 319156320
 	int ancho, alto, C_X1, C_Y1, C_X2, C_Y2;
 	float porcentaje_X, porcentaje_Y, porcentaje_ancho, porcentaje_alto;
-
+           //Un comentario desde github por Zaira Paola Cruz Ramirez
 	scanf_s("%i", &ancho);
 	scanf_s("%i", &alto);
 	scanf_s("%f", &porcentaje_X);
